b4da7e35bf9788459ec9c0726b841e2392af6ac8
AkasHZamaN <akashzaman4@gmail.com>
2022-05-05 05:10:33 +0600

    individual order find

--- index.js
+++ index.js
@@ -83,6 +83,15 @@ async function run() {
         });
         
         // Order collection API
+        app.get('/order', async(req, res)=>{
+            const email = req.query.email;
+            console.log(email)
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.post('/order', async(req, res)=>{
             const order = req.body;
             const result = await orderCollection.insertOne(order);
